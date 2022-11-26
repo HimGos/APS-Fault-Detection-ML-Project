57ede0c39abc89f4a115d781fc61bb6993f7ac60
Himanshu <himgos@gmail.com>
2022-11-26 17:16:40 +0530

    This is the first version of code

---
+++ data_dump.py
@@ -0,0 +1,23 @@
+import pymongo
+import pandas as pd
+import json
+# Provide the mongodb localhost url to connect python to mongodb.
+client = pymongo.MongoClient("mongodb://localhost:27017/neurolabDB")
+
+DATA_FILE_PATH = "/config/workspace/aps_failure_training_set1.csv"
+DATABASE_NAME = "aps"
+COLLECTION_NAME = "sensor"
+
+
+if __name__ == "__main__":
+    df = pd.read_csv(DATA_FILE_PATH)
+    print(f"Rows and columns: {df.shape}")
+
+    # Convert Dataframe to json format so that we can dump these record in mongodb
+    df.reset_index(drop=True, inplace=True)
+
+    json_record = list(json.loads(df.T.to_json()).values())     #This single line of code can convert whole data into json
+    print(json_record[0])
+
+    # insert convereted json record to mongodb
+    client[DATABASE_NAME][COLLECTION_NAME].insert_many(json_record)
