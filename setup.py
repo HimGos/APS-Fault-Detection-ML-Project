04c598c090d7e270a73217dc5374bf8d8fe051e9
Himanshu <himgos@gmail.com>
2022-11-30 08:35:51 +0530

    updated the requirements.txt file

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from setuptools import find_packages, setup
 from typing import List
-
+# find_packages will search for folder and convert it into package to be used as library. In our case our folder is 'sensor'.
+# Any folder containing __init__ file will be considered as a package for find_packages.
 
 def get_requirements()->List[str]:...
+# this function provides list of library names that is required for this project
 
 
 setup(
