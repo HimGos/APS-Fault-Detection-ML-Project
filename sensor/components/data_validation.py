db7f889ed0df0cea89de0526cc68b57eecdb44f8
Himanshu <himgos@gmail.com>
2022-12-03 18:37:28 +0530

    updated data validation

--- sensor/components/data_validation.py
+++ sensor/components/data_validation.py
@@ -5,6 +5,7 @@ import os, sys
 from scipy.stats import ks_2samp
 from typing import Optional
 import pandas as pd
+import yaml
 
 class DataValidation:
 
@@ -12,6 +13,7 @@ class DataValidation:
         try:
             logging.info(f"{'>>'*20} Data Validation {'<<'*20}")
             self.data_validation_config = data_validation_config
+            self.validation_error = dict()
         except Exception as e:
             raise SensorException(e,sys)
 
@@ -27,10 +29,11 @@ class DataValidation:
         
         """
         try:
+            threshold = self.data_validation_config.missing_threshold
             null_report = df.isna().sum()/df.shape[0]
-
             #selecting column name which contains null values
             drop_column_names = null_report[null_report>0.3].index
+            self.validation_error["dropped_columns"] = drop_column_names
             df.drop(list(drop_column_names), axis=1, inplace=True)
 
             #return None no column left
@@ -41,13 +44,67 @@ class DataValidation:
             raise SensorException(e, sys)
 
 
-    def is_required_columns_exists(self,base_df, present_df)->bool:
+    def is_required_columns_exists(self, base_df:pd.DataFrame, current_df:pd.DataFrame)->bool:
+        try:
+            base_columns = base_df.columns
+            current_columns = current_df.columns
+
+            missing_columns = []
+            for base_column in base_columns:
+                if base_column not in current_columns:
+                    missing_columns.appent(base_column)
+
+                
+            if len(missing_columns)>0:
+                self.validation_error["Missing columns"] = missing_columns
+                return False
+            return True
+
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def data_drift(self, base_df:pd.DataFrame, current_df:pd.DataFrame):
+        try:
+            drift_report = dict()
+
+            base_columns = base_df.columns
+            current_columns = current_df.columns
+
+            for base_column in base_columns:
+                base_data, current_data = base_df[base_column], current_df[base_column]
+                #Null hypohesis is that both columns data drawn from same distribution
+                same_distribution = ks_2samp(base_data, current_data)
+
+                if same_distribution.pvalue>0.05:
+                    #We are accepting null hypothesis
+                    drift_report[base_column]= {
+                        "pvalues":same_distribution.pvalue,
+                        "same_distribution":True
+                    }
+                    #same distribution
+                else:
+                    drift_report[base_column]={
+                        "pvalues":same_distribution.pvalue,
+                        "same_distribution":False
+                    }
+
+        except Exception as e:
+            raise SensorException(e, sys)
+
+    def initiate_data_validation(self)->artifact_entity.DataValidationArtifact:
         try:
             pass
 
         except Exception as e:
             raise SensorException(e, sys)
 
+    def write_yaml_file(file_path, data:dict):
+        try:
+            file_dir = os.path.dirname(file_path)
 
-    def initiate_data_validation(self)->artifact_entity.DataValidationArtifact:
-        pass
+            os.makedirs(file_dir, exist_ok=True)
+            with open(file_path,"w") as file_writer:
+                yaml.dump(data,file_writer)
+
+        except Exception as e:
+            raise SensorException(e, sys)
--- sensor/utils.py
+++ sensor/utils.py
@@ -3,9 +3,8 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.config import mongo_client
 import os,sys
-
-def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.DataFrame:
-    
+import yaml
+def get_collection_as_dataframe(database_name:str,collection_name:str)->pd.DataFrame:
     """
     Description: This function return collection as dataframe
     =========================================================
@@ -15,9 +14,8 @@ def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.Data
     =========================================================
     return Pandas dataframe of a collection
     """
-    
     try:
-        logging.info(f"Reading data from Database: {database_name} and collection: {collection_name}")
+        logging.info(f"Reading data from database: {database_name} and collection: {collection_name}")
         df = pd.DataFrame(list(mongo_client[database_name][collection_name].find()))
         logging.info(f"Found columns: {df.columns}")
         if "_id" in df.columns:
@@ -27,3 +25,22 @@ def get_collection_as_dataframe(database_name:str, collection_name:str)->pd.Data
         return df
     except Exception as e:
         raise SensorException(e, sys)
+    
+
+def write_yaml_file(file_path,data:dict):
+    try:
+        file_dir = os.path.dirname(file_path)
+        os.makedirs(file_dir,exist_ok=True)
+        with open(file_path,"w") as file_writer:
+            yaml.dump(data,file_writer)
+    except Exception as e:
+        raise SensorException(e, sys)
+
+def convert_columns_float(df:pd.DataFrame,exclude_columns:list)->pd.DataFrame:
+    try:
+        for column in df.columns:
+            if column not in exclude_columns:
+                df[column]=df[column].astype('float')
+        return df
+    except Exception as e:
+        raise e
